cc518c891305d29a1494635b747b169d1f63fa01
antoniotm22 <antonio1hua@gmail.com>
2020-11-19 14:24:08 +0100

    UP version 2

--- src/version1/UnderTheSea.java
+++ src/version1/UnderTheSea.java
@@ -2,8 +2,11 @@ package version1;
 
 import java.awt.BorderLayout;
 import java.awt.Canvas;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class UnderTheSea extends Canvas {
@@ -19,26 +22,47 @@ public class UnderTheSea extends Canvas {
 	JFrame ventana = new JFrame("UnderTheSea");
 
 	public UnderTheSea() {
-		
-		 JPanel panel = (JPanel) ventana.getContentPane();
-		 
-		 // Establezco el tipo de panel para añadir el canvas
-		 panel.setLayout(new BorderLayout());
-		 // Añado el tipo de panel
-		 panel.add(this,BorderLayout.CENTER);
-		 
+
+		JPanel panel = (JPanel) ventana.getContentPane();
+
+		// Establezco el tipo de panel para añadir el canvas
+		panel.setLayout(new BorderLayout());
+		// Añado el tipo de panel
+		panel.add(this, BorderLayout.CENTER);
+
 		// Establecemos las medidas de la ventana.
-			ventana.setBounds(0, 0, WIDTH, HEIGTH);
+		ventana.setBounds(0, 0, WIDTH, HEIGTH);
 
 		// Hacemos que la ventana sea visible
 		ventana.setVisible(true);
 
 		// Permite que no se refresque automaticamente, en cambio hay que hacerlo
 		// automaticamente.
-		ventana.setAutoRequestFocus(false);
+		ventana.setIgnoreRepaint(true);
 		// Nos permitirá que el foco de atencion se vaya a la aplicación.
 		this.requestFocus();
 
+		// Esto nos permite interceptar cuando quieren cerrar la ventana y ejecutamos un
+		// listener con un adaptador, como es una interfaz debemos de instanciarlo de
+		// una forma diferente
+		ventana.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				cierraAplicacion();
+			}
+		});
+	}
+
+	/**
+	 * Método que contiene un array de opciones, en caso de que sea una respuesta
+	 * positiva se saldrá, sino permanezerá dentro del juego.
+	 */
+	private void cierraAplicacion() {
+		String[] opciones = { "Aceptar", "Cancelar" };
+		int eleccion = JOptionPane.showOptionDialog(ventana, "�Desea cerrar la aplicaci�n?", "Salir de la aplicaci�n",
+				JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, opciones, "Aceptar");
+		if (eleccion == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
 	}
 
 	/**
